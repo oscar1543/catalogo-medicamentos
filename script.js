a91fedd8783a52b46350d4737a6d52c04026ac62
oscar1543 <oscarncmx@gmail.com>
2025-07-24 21:46:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const productos = [
   {
     nombre: "Xtandi 40mg",
     precio: "$80,000.00",
-    descripcion: "Enzalutamida 40mg",
-    descripcion: "Caja con 120 cápsulas",
-    descripcion: "Laboratorio:Astellas",
+    descripcion: "Enzalutamida 40mg",descripcion: "Caja con 120 cápsulas",descripcion: "Laboratorio:Astellas",
     imagen: "https://dstamaria.com/cdn/shop/products/xtandi-40.png?v=1740597947&width=493"
   },
   {
